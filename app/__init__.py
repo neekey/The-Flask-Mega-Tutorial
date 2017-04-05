7237e3a82b255e7aa8c3b5622ce6cd3c2aea4ca3
neekey <ni184775761@gmail.com>
2017-04-05 17:24:21 +1000

    try to add openId login

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
 
+lm = LoginManager()
 app = Flask(__name__)
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
--- config.py
+++ config.py
@@ -14,3 +14,4 @@ OPENID_PROVIDERS = [
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
