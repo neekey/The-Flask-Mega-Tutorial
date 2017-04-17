0bd43244b7ce47bd480d8770508db698707abcdc
neekey <ni184775761@gmail.com>
2017-04-17 12:45:34 +1000

    add about me

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import login_user, current_user, login_required, logout_user
 from .models import User
 from .OAuth import OAuthSignIn
+from datetime import datetime
+from .forms import EditForm
 
 
 @lm.user_loader
@@ -77,3 +79,41 @@ def logout():
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'},
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
+
